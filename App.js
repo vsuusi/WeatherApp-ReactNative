5000e1c48e2699e1f55a40cf8153a6bb806338fc
Santeri Uusitalo <vs.uusitalo@gmail.com>
2024-03-05 18:16:43 +0200

    initalized project

--- App.js
+++ App.js
@@ -2,19 +2,39 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+/* 
+  - web palvelu/REST api
+  - kaksi näkymää
+  - laiterajapinta esim paikkatieto, kartat, sensorit
+  - varaudu käyttöliittymässä virhetilanteisiin
+  - listanäkymä
+*/
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text>Halojatahalloo</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.container2}>
+        <Text>Halojatahalloo</Text>
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container2: {
+    flex: 1,
+    backgroundColor: '#f0f',
     alignItems: 'center',
     justifyContent: 'center',
   },
